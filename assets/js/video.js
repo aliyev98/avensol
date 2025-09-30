933fc10a01ee3f451c3a54e94447f465678bbfd2
aliyev98 <maharram.aliyev98@gmail.com>
2025-09-30 10:59:42 +0400

    section title font size

--- assets/js/video.js
+++ assets/js/video.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const title = el.dataset.title || 'YouTube video';
             const iframe = document.createElement('iframe');
 
-            iframe.src = `https://www.youtube-nocookie.com/embed/${id}?autoplay=1&rel=0&modestbranding=1&playsinline=1`;
+            iframe.src="https://www.youtube-nocookie.com/embed/FV4e5tcitIs?rel=0&loop=1&playlist=FV4e5tcitIs&modestbranding=1&playsinline=1&autoplay=1&mute=1";
             iframe.title = title;
             iframe.allow =
                 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
